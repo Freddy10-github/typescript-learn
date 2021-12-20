1277d5dc0ba3f6a8df0389254f2564f86353d443
Freddylo <freddy_luo@moneynet.tw>
2021-12-20 14:25:35 +0800

    type-inference

---
+++ D02-type-inference-and-annotation/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// export {};
+// let name = 'Freddy';                          // string
+let age = 24; // number
+let nothing = undefined; // any
+let hasPet = true; // boolean
+let nothingLiterally = null; // any
+console.log(name);
---
+++ D02-type-inference-and-annotation/index.ts
@@ -0,0 +1,8 @@
+export {};  // for name 
+
+let name = 'Freddy';                            // string
+let age = 24;                                   // number
+let nothing = undefined;                        // any
+let hasPet = true;                              // boolean
+let nothingLiterally = null;                    // any
+
